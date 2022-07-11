92449833b428b1ff81057bbd2d714c603abf509f
liu-yucheng <liu.r.yucheng@gmail.com>
2022-07-11 21:05:59 +0800

    Cleaned-up NavPages code

--- ktutils/build.gradle.kts
+++ ktutils/build.gradle.kts
@@ -8,7 +8,7 @@ import org.jetbrains.dokka.gradle.DokkaTask
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 group = "lyc"
-version = "1.1.1"
+version = "1.1.2"
 val jarBaseName = "ktutils"
 
 // Add version.properties to the JAR resources
