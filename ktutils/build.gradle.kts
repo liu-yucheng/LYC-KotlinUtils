486579fc64a7e2e30174e6938de4716843234552
liu-yucheng <liu.r.yucheng@gmail.com>
2022-07-19 03:51:26 +0800

    Updated Navs

--- ktutils/build.gradle.kts
+++ ktutils/build.gradle.kts
@@ -8,7 +8,7 @@ import org.jetbrains.dokka.gradle.DokkaTask
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 group = "lyc"
-version = "1.2.4"
+version = "1.2.5"
 val jarBaseName = "ktutils"
 
 // Add version.properties to the JAR resources
